d99d0fdf55a847c9c128ba6c28fadddb1b749732
Odhran <mahonyodhran@gmail.com>
2022-06-15 14:34:34 +0100

    added functionality to get Guests and display on page

---
+++ src/main/java/com/odhranmahony/lil/learningspring/web/GuestController.java
@@ -0,0 +1,24 @@
+package com.odhranmahony.lil.learningspring.web;
+
+import com.odhranmahony.lil.learningspring.business.ReservationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestController {
+    private final ReservationService reservationService;
+
+    public GuestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getGuests(Model model){
+        model.addAttribute("guests", this.reservationService.getHotelGuests());
+        //call it hotel-guests as 'guests' may have circular reference
+        return "hotel-guests";
+    }
+}
