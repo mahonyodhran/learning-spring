81590e7ffad9a97f2db9a690a85c95b7541bfa38
Odhran <mahonyodhran@gmail.com>
2022-06-15 15:03:26 +0100

    Expose service layer through REST

---
+++ src/main/java/com/odhranmahony/lil/learningspring/webservice/WebserviceController.java
@@ -0,0 +1,30 @@
+package com.odhranmahony.lil.learningspring.webservice;
+
+import com.odhranmahony.lil.learningspring.business.ReservationService;
+import com.odhranmahony.lil.learningspring.business.RoomReservation;
+import com.odhranmahony.lil.learningspring.util.DateUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebserviceController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public WebserviceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(@RequestParam(value = "date", required = false)String dateString){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        return this.reservationService.getRoomReservationsForDate(date);
+    }
+}
