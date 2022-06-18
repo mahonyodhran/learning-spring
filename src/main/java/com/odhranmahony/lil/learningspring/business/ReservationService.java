edb409c654d61aa342e0cd31fcbd39096ff82349
Odhran <mahonyodhran@gmail.com>
2022-06-18 16:26:19 +0100

    updated type of HashMap

--- src/main/java/com/odhranmahony/lil/learningspring/business/ReservationService.java
+++ src/main/java/com/odhranmahony/lil/learningspring/business/ReservationService.java
@@ -11,15 +11,16 @@ public class ReservationService {
     private final GuestRepository guestRepository;
     private final ReservationRepository reservationRepository;
 
-    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository,
+            ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
         this.guestRepository = guestRepository;
         this.reservationRepository = reservationRepository;
     }
 
-    public List<RoomReservation> getRoomReservationsForDate(Date date){
+    public List<RoomReservation> getRoomReservationsForDate(Date date) {
         Iterable<Room> rooms = this.roomRepository.findAll();
-        Map<Long, RoomReservation> roomReservationMap = new HashMap();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<Long, RoomReservation>();
         rooms.forEach(room -> {
             RoomReservation roomReservation = new RoomReservation();
             roomReservation.setRoomId(room.getId());
@@ -27,7 +28,8 @@ public class ReservationService {
             roomReservation.setRoomNumber(room.getRoomNumber());
             roomReservationMap.put(room.getId(), roomReservation);
         });
-        Iterable<Reservation> reservations = this.reservationRepository.findReservationByReservationDate(new java.sql.Date(date.getTime()));
+        Iterable<Reservation> reservations = this.reservationRepository
+                .findReservationByReservationDate(new java.sql.Date(date.getTime()));
         reservations.forEach(reservation -> {
             RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
             roomReservation.setDate(date);
@@ -43,7 +45,7 @@ public class ReservationService {
         roomReservations.sort(new Comparator<RoomReservation>() {
             @Override
             public int compare(RoomReservation o1, RoomReservation o2) {
-                if (o1.getRoomName().equals(o2.getRoomName())){
+                if (o1.getRoomName().equals(o2.getRoomName())) {
                     return o1.getRoomNumber().compareTo(o2.getRoomNumber());
                 }
                 return o1.getRoomName().compareTo(o2.getRoomName());
@@ -52,14 +54,16 @@ public class ReservationService {
         return roomReservations;
     }
 
-    public List<Guest> getHotelGuests(){
+    public List<Guest> getHotelGuests() {
         Iterable<Guest> guests = this.guestRepository.findAll();
         List<Guest> guestList = new ArrayList<>();
-        guests.forEach(guest -> {guestList.add(guest);});
+        guests.forEach(guest -> {
+            guestList.add(guest);
+        });
         guestList.sort(new Comparator<Guest>() {
             @Override
             public int compare(Guest o1, Guest o2) {
-                if (o1.getLastName().equals(o2.getLastName())){
+                if (o1.getLastName().equals(o2.getLastName())) {
                     return o1.getFirstName().compareTo(o2.getFirstName());
                 }
                 return o1.getLastName().compareTo(o2.getLastName());
@@ -69,7 +73,7 @@ public class ReservationService {
     }
 
     public void addGuest(Guest guest) {
-        if(null == guest){
+        if (null == guest) {
             throw new RuntimeException("Guest cannot be null");
         }
         this.guestRepository.save(guest);
@@ -78,7 +82,9 @@ public class ReservationService {
     public List<Room> getRooms() {
         Iterable<Room> rooms = this.roomRepository.findAll();
         List<Room> roomList = new ArrayList<>();
-        rooms.forEach(room -> {roomList.add(room);});
+        rooms.forEach(room -> {
+            roomList.add(room);
+        });
         roomList.sort(new Comparator<Room>() {
             @Override
             public int compare(Room o1, Room o2) {
