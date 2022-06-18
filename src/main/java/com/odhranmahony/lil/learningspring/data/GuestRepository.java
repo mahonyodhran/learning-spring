ad1ef5c837f323667930fb36289048163ed949ea
Odhran <mahonyodhran@gmail.com>
2022-06-18 16:26:37 +0100

    removed unused import

--- src/main/java/com/odhranmahony/lil/learningspring/data/GuestRepository.java
+++ src/main/java/com/odhranmahony/lil/learningspring/data/GuestRepository.java
@@ -1,7 +1,6 @@
 package com.odhranmahony.lil.learningspring.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
